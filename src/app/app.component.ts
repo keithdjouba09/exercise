7fd043989304fe06f79f1b470471f8d0e58bd76d
keithdjouba09 <marlondjouba09@gmail.com>
2018-05-17 21:10:57 -0500

    adding service, design, app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { SearchService } from './search.service';
+import { Subject } from 'rxjs/Subject';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SearchService]
 })
 export class AppComponent {
-  title = 'app workss!';
+  results: Object;
+  searchTerm$ = new Subject<string>();
+  //title = 'app works!';
+  constructor(private searchService: SearchService) {
+    this.searchService.search(this.searchTerm$)
+      .subscribe(results => {
+        this.results = results.results;
+      });
+  }
 }

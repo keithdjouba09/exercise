ff5a06db7b59901fa243f40c6d0e4cb67497e771
keithdjouba09 <marlondjouba09@gmail.com>
2018-05-21 08:06:35 -0500

    add modals and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SearchService } from './search.service';
 import { Subject } from 'rxjs/Subject';
+import {Popup} from 'ng2-opd-popup';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,10 +12,14 @@ export class AppComponent {
   results: Object;
   searchTerm$ = new Subject<string>();
   //title = 'app works!';
-  constructor(private searchService: SearchService) {
+  constructor(private searchService: SearchService,private popup: Popup) {
     this.searchService.search(this.searchTerm$)
       .subscribe(results => {
         this.results = results.results;
       });
+
+  }
+  ClickButton(){
+    this.popup.show();
   }
 }
